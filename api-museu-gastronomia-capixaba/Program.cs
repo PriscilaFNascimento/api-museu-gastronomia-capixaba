d4f99f941c4484ed1647eefac6eb20a4b40407cd
Priscila <fnas.priscila@gmail.com>
2023-09-07 16:18:55 -0300

    feat: tratativa de enums e ajustes retorno de detalhes da receita

--- api-museu-gastronomia-capixaba/Program.cs
+++ api-museu-gastronomia-capixaba/Program.cs
@@ -5,12 +5,17 @@ using Domain.Repositories;
 using Domain.Services;
 using Domain.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x => {
+    // serialize enums as strings in api responses (e.g. Role)
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
